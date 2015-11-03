8552705bf5b22bdf9984dac8d3a6ccddf031e485
Emil Matubis <outlime@users.noreply.github.com>
2015-11-03 19:39:59 +0800

    Remsys

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,43 +12,46 @@
 	<link rel="stylesheet" type="text/css" href="{{ URL::asset('css/add.css') }}">	
 	<link href='https://fonts.googleapis.com/css?family=Titillium+Web' rel='stylesheet' type='text/css'>
 	
-
 	<title>Reminderz</title>
 </head>
 <body>
 	<div class="container">
-		
 		<div class="jumbotron">
-			{{-- Error Messages --}}
-			@include('partials.messages')
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						{{-- Error Messages --}}
+						@include('partials.messages')
 
-			<h2>Reminder System</h2>
-			<hr>
-			
-			{{-- Input form --}}
-			<form method="POST" action="newpost">
-		        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-		        <div class="form-group">
-		            <input type="email" name="email" required="required" class="form-control" placeholder="Enter your email" value="">
-		        </div>
-		        <div class="form-group">
-		            <input type="date" name="date" required="required" class="form-control" value="{{ Carbon\Carbon::now()->format('Y-m-d') }}">
-		        </div>
-		        <div class="form-group">
-		            <input type="time" name="time" required="required" class="form-control" value="{{ Carbon\Carbon::now()->format('H:i') }}">
-		        </div>
-		        <div class="form-group">
-		            <textarea rows="3" name="reminder" required="required" class="form-control" placeholder="Reminder..."></textarea>
-		        </div>
-		        <hr>
-		        <div class="form-group">
-		            <button type="submit" class="btn btn-remind">Remind Me!</button>
-		        </div>
-		    </form>
+						<h2>Reminder System</h2>
+						<hr>
+
+						{{-- Input form --}}
+						<form method="POST" action="newpost">
+					        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+					        <div class="form-group">
+					            <input type="email" name="email" required="required" class="form-control" placeholder="Enter your email" value="">
+					        </div>
+					        <div class="form-group">
+					            <input type="date" name="date" required="required" class="form-control" value="{{ Carbon\Carbon::now()->format('Y-m-d') }}">
+					        </div>
+					        <div class="form-group">
+					            <input type="time" name="time" required="required" class="form-control" value="{{ Carbon\Carbon::now()->format('H:i') }}">
+					        </div>
+					        <div class="form-group">
+					            <textarea rows="3" name="reminder" required="required" class="form-control" placeholder="Reminder..."></textarea>
+					        </div>
+					        <hr>
+					        <div class="form-group">
+					            <button type="submit" class="btn btn-remind">Remind Me!</button>
+					        </div>
+					    </form>
+				    </div>
+			    </div>
+		    </div>
 	    </div>
 
 	    <h4 class="foot">Copyright &copy; 2015 Wansi.</h4>
-
 	</div>
 
 	<script src="{{ URL::asset('js/jquery.min.js') }}"></script>
